3705e5d4ee33d6650a28c50fc750ef6df837fa64
Lammert Westerhoff <westerhoff@gmail.com>
2014-10-24 20:23:23 +0200

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name a1SiteApp
+ * @description
+ * # a1SiteApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('a1SiteApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name a1SiteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the a1SiteApp
+ */
+angular.module('a1SiteApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
